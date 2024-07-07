2c4f4ed6151d111960ab875f0b1e4cedd1f6839b
elized12 <80714855+elized12@users.noreply.github.com>
2024-07-07 06:36:02 +0300

    -FINISHED PAGE INFO
    finished info product page

--- Servistair_Bank_and_Info/script/script_info_product.js
+++ Servistair_Bank_and_Info/script/script_info_product.js
@@ -24,9 +24,6 @@ controllers.forEach((controller) => {
   });
 });
 
-
-
-
 function uncoverText() {
   const buttonDescription = document.querySelector(
     ".description__button-open-close"
@@ -44,6 +41,22 @@ function uncoverText() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  let swiperIntroSlider = new Swiper(".intro-slider", {
+    navigation: {
+      nextEl: ".intro-slider-button-next",
+      prevEl: ".intro-slider-button-prev",
+    },
+  });
+
+  const swiperInfoSlider = initilizationSliderInformaiton(swiperIntroSlider);
+
+  swiperIntroSlider.on("slideChange", function () {
+    console.log(swiperInfoSlider.realIndex);
+    resetActiveSlider();
+    setActiveSlider(this.realIndex);
+    swiperInfoSlider.slideTo(this.realIndex);
+  });
+
   const swiperRecommendedSlider = new Swiper(".recommended-slider", {
     slidesPerView: "auto",
     spaceBetween: 20,
@@ -64,12 +77,90 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   });
-  const swiperIntroSlider = new Swiper(".intro-slider", {
-    navigation: {
-      nextEl: ".intro-slider-button-next",
-      prevEl: ".intro-slider-button-prev",
+
+  const swiperMainSlider = new Swiper(".main-slider__content", {
+    centeredSlides: true,
+    slidesPerView: 1,
+    pagination: {
+      el: ".main-slider__pagination",
     },
+    loop: true,
+  });
+});
+
+function initilizationSliderInformaiton(sliderIntro) {
+  let introSlider = document.querySelector(".intro-slider");
+
+  let imagesInnerIntroSlider = introSlider.querySelectorAll(".intro-image");
+
+  let countElementIntroSlider = imagesInnerIntroSlider.length;
+
+  for (let i = 0; i < countElementIntroSlider; i++) {
+    createElementForSliderBy(imagesInnerIntroSlider[i], i, sliderIntro);
+  }
+
+  let countVisitedSlide = 0;
+
+  if (countElementIntroSlider != 0) {
+    setActiveSlider(0);
+  }
+
+  if (countElementIntroSlider <= 2) {
+    beetween = 0;
+    let slider = document.querySelector(".block-information__slider");
+    slider.style.maxWidth = "180px";
+    countVisitedSlide = countElementIntroSlider;
+  } else {
+    countVisitedSlide = 3;
+  }
+
+  const swiperInfoSlider = new Swiper(".block-information__slider", {
+    slidesPerView: countVisitedSlide,
   });
 
+  return swiperInfoSlider;
+}
 
-});
+function createElementForSliderBy(element, index, slider) {
+  const swiperSlide = document.createElement("div");
+  swiperSlide.classList.add("swiper-slide", "information-slider__item");
+
+  const imageBox = document.createElement("div");
+  imageBox.classList.add("block-information__slider-image-box");
+
+  const img = document.createElement("img");
+  img.src = element.src;
+  img.alt = element.alt;
+  img.classList.add("block-information__slider-image");
+
+  imageBox.appendChild(img);
+
+  swiperSlide.appendChild(imageBox);
+
+  const parent = document.querySelector(".information-slider__wrapper");
+  parent.appendChild(swiperSlide);
+
+  swiperSlide.addEventListener("click", function () {
+    slider.slideTo(index);
+    resetActiveSlider();
+    setActiveSlider(index);
+  });
+
+  return swiperSlide;
+}
+
+function resetActiveSlider() {
+  let sliders = document.querySelectorAll(".information-slider__item");
+
+  for (let i = 0; i < sliders.length; i++) {
+    if (sliders[i].classList.contains("active-slider-image-box")) {
+      sliders[i].classList.remove("active-slider-image-box");
+    }
+  }
+}
+
+function setActiveSlider(index) {
+  let sliders = document.querySelectorAll(".information-slider__item");
+
+  sliders[index].classList.add("active-slider-image-box");
+}
