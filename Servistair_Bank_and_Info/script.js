91f753e8fd98dd87392a7024f866bf581e633e12
elized12 <80714855+elized12@users.noreply.github.com>
2024-07-09 04:04:30 +0300

    -UPDATE PAGE BANK ADD CALCUCATOR
    add calculator in page Bank

--- Servistair_Bank_and_Info/script.js
+++ Servistair_Bank_and_Info/script.js
@@ -1,3 +1,8 @@
+//GLOBAL VARS
+
+let sumDelivery = 0;
+let textMethodPay;
+
 function clearButtonDelivery() {
   var buttons = document.querySelectorAll(".address__button");
 
@@ -14,7 +19,21 @@ function setActiveButtonDelivery(element) {
 
   const radioBtn = element.querySelector('input[type="radio"]');
 
+  let elementPrice = element.querySelector(".button__price");
+
+  let price = elementPrice.innerHTML;
+
+  if (price == "бесплатно") {
+    sumDelivery = 0;
+  } else {
+    price = price.replace(/₽/g, "");
+
+    sumDelivery = parseFloat(price);
+  }
+
   radioBtn.checked = true;
+
+  calculatePrise();
 }
 
 //ALL input
@@ -124,6 +143,8 @@ function deleteProduct(element) {
       cardProduct.remove();
     }
   });
+
+  calculatePrise();
 }
 
 function addCountProduct(element) {
@@ -134,11 +155,36 @@ function addCountProduct(element) {
       var inputProductCount = cardProduct.querySelector(
         ".button-add-pop__input"
       );
-      console.log(inputProductCount);
+
+      let priceForOne = parseFloat(
+        cardProduct.querySelector(".price-for-one").innerHTML
+      );
+
+      let cardProductPrice = cardProduct.querySelector(".card__price");
+
+      let stringProduct = cardProductPrice.innerHTML;
+
+      console.log(stringProduct);
+
+      stringProduct = stringProduct.replace(
+        /<span class="price__valuta">руб.<\/span>/g,
+        ""
+      );
+
+      console.log(stringProduct);
+
+      let priceProduct = parseFloat(stringProduct);
 
       inputProductCount.value++;
+
+      let resultPrice = priceForOne * parseFloat(inputProductCount.value);
+
+      cardProductPrice.innerHTML =
+        String(resultPrice) + ' <span class="price__valuta">руб.</span>';
     }
   });
+
+  calculatePrise();
 }
 
 function erraseCountProduct(element) {
@@ -149,14 +195,37 @@ function erraseCountProduct(element) {
       var inputProductCount = cardProduct.querySelector(
         ".button-add-pop__input"
       );
+
+      let priceForOne = parseFloat(
+        cardProduct.querySelector(".price-for-one").innerHTML
+      );
+
+      let cardProductPrice = cardProduct.querySelector(".card__price");
+
+      let stringProduct = cardProductPrice.innerHTML;
+
+      console.log(stringProduct);
+
+      console.log(stringProduct);
+
       console.log(inputProductCount);
 
       inputProductCount.value--;
 
       if (inputProductCount.value <= 0) deleteProduct(cardProduct);
+      else {
+        let resultPriceProduct =
+          priceForOne * parseFloat(inputProductCount.value);
+        cardProductPrice.innerHTML =
+          String(resultPriceProduct) +
+          ' <span class="price__valuta">руб.</span>';
+      }
     }
   });
+  calculatePrise();
 }
+
+//input for count products
 
 const inputCountProducts = document.querySelectorAll(".button-add-pop__input");
 
@@ -166,7 +235,39 @@ inputCountProducts.forEach((input) => {
 
     count = count.replace(/\D/g, "");
 
-    event.target.value = count;
+    if (count === "" || isNaN(count)) {
+      event.target.value = "1";
+      count = "1";
+    } else if (parseInt(count) > 100) {
+      event.target.value = "100";
+      count = "100";
+    } else {
+      event.target.value = count;
+    }
+
+    let cards = document.querySelectorAll(".order__card");
+
+    cards.forEach(function (card) {
+      if (card.contains(input)) {
+        let priceForOne = parseFloat(
+          card.querySelector(".price-for-one").innerHTML
+        );
+
+        if (isNaN(count)) {
+          count = parseFloat(1);
+          console.log(count);
+        }
+
+        let resultPrice = priceForOne * parseFloat(count);
+
+        let cardPrice = card.querySelector(".card__price");
+
+        cardPrice.innerHTML =
+          String(resultPrice) + ' <span class="price__valuta">руб.</span>';
+
+        calculatePrise();
+      }
+    });
   });
 });
 
@@ -227,9 +328,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   checkResolution();
 
+  calculatePrise();
+
   window.addEventListener("resize", checkResolution);
 });
 
+//function for slider
 function initializeButtonSlider(slider) {
   const cardButtonPrev = document.querySelector(".cards-button-prev");
 
@@ -260,3 +364,83 @@ function removeSwiperNotifications() {
     notification.remove();
   });
 }
+
+function calculatePrise() {
+  let sumResult = 0;
+  let countResult = 0;
+
+  const cardsProducts = document.querySelectorAll(".order__card");
+
+  cardsProducts.forEach(function (card) {
+    let innerCardProduct = card.querySelector(".card__price").innerHTML;
+
+    let inputPopAdd = card.querySelector(".button-add-pop__input");
+
+    let count = parseInt(inputPopAdd.value);
+
+    let price = innerCardProduct.replace(
+      /<span class="price__valuta">руб\.<\/span>/g,
+      ""
+    );
+
+    price = parseFloat(price);
+
+    sumResult += price;
+    countResult += count;
+  });
+
+  const totalPirce = document.querySelector(".total__tovars");
+  const countProductsOrder = document.querySelector(".total__count-products");
+  const totalDelivery = document.querySelector(".total__delivery");
+
+  console.log(sumResult);
+
+  if (sumDelivery == 0) {
+    totalDelivery.style.display = "none";
+  } else {
+    totalDelivery.style.display = "flex";
+
+    const deliveryPrice = document.querySelector(".total-price-delivery");
+    deliveryPrice.innerHTML = String(sumDelivery) + " ₽";
+  }
+
+  if (countResult == 0) {
+    totalPirce.style.display = "none";
+  } else {
+    totalPirce.style.display = "flex";
+
+    countProductsOrder.innerHTML = String(countResult) + " Товара";
+
+    const priceOrder = document.querySelector(".total__price");
+    priceOrder.innerHTML = String(sumResult) + " ₽";
+  }
+
+  const totalOrderPrice = document.querySelector(".total-result-price");
+
+  totalOrderPrice.innerHTML = String(sumDelivery + sumResult) + " ₽";
+}
+
+//function for text method pay
+
+function setMethodPay(numberMethod) {
+  if (numberMethod == 1) {
+    textMethodPay = "Безнал";
+  } else if (numberMethod == 2) {
+    textMethodPay = "оплата картой";
+  } else if (numberMethod == 3) {
+    textMethodPay = "наличными";
+  }
+
+  const elementTotalMethod = document.querySelector(".total__method");
+
+  elementTotalMethod.innerHTML = textMethodPay;
+}
+
+const buttonsMethodPay = document.querySelectorAll(".payment__input");
+
+buttonsMethodPay.forEach(function (buttonMethod) {
+  buttonMethod.addEventListener("change", function () {
+    const totalMethod = document.querySelector(".total__method");
+    totalMethod.style.display = "block";
+  });
+});
